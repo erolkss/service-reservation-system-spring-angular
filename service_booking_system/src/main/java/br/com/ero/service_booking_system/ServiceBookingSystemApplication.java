d838e859aaf1bf2be05d7fedaa031f9db4c00876
Lucas Lima <lucaserolima@gmail.com>
2024-04-13 16:52:42 -0400

    Create a project Spring 'service_booking_system' - Backend

---
+++ service_booking_system/src/main/java/br/com/ero/service_booking_system/ServiceBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.ero.service_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBookingSystemApplication.class, args);
+	}
+
+}
---
+++ service_booking_system/src/test/java/br/com/ero/service_booking_system/ServiceBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.service_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
