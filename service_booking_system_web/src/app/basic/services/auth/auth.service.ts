c3b2b6aa7b0477e07ed2aa80e0910cda821b8f6c
Lucas Lima <lucaserolima@gmail.com>
2024-04-13 21:37:25 -0400

    feat: Creating Sign-up Company and Client Methods in Angular - Frontend

--- service_booking_system_web/src/app/basic/services/auth/auth.service.ts
+++ service_booking_system_web/src/app/basic/services/auth/auth.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASIC_URL = 'http://localhost:8080/'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  registerClient(singnupRequestDTO: any): Observable<any>{
+    return this.http.post(BASIC_URL + 'client/sign-up', singnupRequestDTO);
+  }
+
+  registerCompany(singnupRequestDTO: any): Observable<any>{
+    return this.http.post(BASIC_URL + 'company/sign-up', singnupRequestDTO);
+  }
 }
