bdaf21b407b4ccca049c141b6e7a9ba6d25f8510
Lucas Lima <lucaserolima@gmail.com>
2024-04-15 22:15:04 -0400

    Update front debug

--- service_booking_system_web/src/app/basic/services/auth/auth.service.ts
+++ service_booking_system_web/src/app/basic/services/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
-const BASIC_URL = 'http://localhost:8080/'
+const BASIC_URL = 'http://localhost:8080/';
+export const AUTH_HEADER = 'authorization';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,24 @@ export class AuthService {
     private http: HttpClient,
   ) { }
 
-  registerClient(singnupRequestDTO: any): Observable<any>{
+  registerClient(singnupRequestDTO: any): Observable<any> {
     return this.http.post(BASIC_URL + 'client/sign-up', singnupRequestDTO);
   }
 
-  registerCompany(singnupRequestDTO: any): Observable<any>{
+  registerCompany(singnupRequestDTO: any): Observable<any> {
     return this.http.post(BASIC_URL + 'company/sign-up', singnupRequestDTO);
+  }
+
+  login(username: string, password: string) {
+    return this.http.post(BASIC_URL + 'authenticate', { username, password }, { observe: 'response' })
+      .pipe(
+        map((res: HttpResponse<any>) => {
+          console.log(res.body);
+          const tokenLength = res.headers.get(AUTH_HEADER)?.length;
+          const bearerToken = res.headers.get(AUTH_HEADER)?.substring(7, tokenLength);
+          console.log(bearerToken);
+          return res;
+        })
+      );
   }
 }
