3628b024d66d00fc3e3d84ac8ee0790a1d862d3e
Lucas Lima <lucaserolima@gmail.com>
2024-04-16 19:48:07 -0400

    feat: Creating a Page with Reactive form to Post new ad in Angular - Frontend

--- service_booking_system_web/src/app/company/company.module.ts
+++ service_booking_system_web/src/app/company/company.module.ts
@@ -4,16 +4,22 @@ import { CommonModule } from '@angular/common';
 import { CompanyRoutingModule } from './company-routing.module';
 import { CompanyComponent } from './company.component';
 import { CompanyDashboardComponent } from './pages/company-dashboard/company-dashboard.component';
+import { CreateAdComponent } from './pages/create-ad/create-ad.component';
+import { DemoNgZorroAntdModule } from '../DemoNgZorroAntdModule';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     CompanyComponent,
-    CompanyDashboardComponent
+    CompanyDashboardComponent,
+    CreateAdComponent,
   ],
   imports: [
     CommonModule,
-    CompanyRoutingModule
+    CompanyRoutingModule,
+    DemoNgZorroAntdModule,
+    ReactiveFormsModule
   ]
 })
 export class CompanyModule { }
