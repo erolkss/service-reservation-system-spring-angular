3a17485bd2b1781f410b2cc9b149572f054a7cad
Lucas Lima <lucaserolima@gmail.com>
2024-04-17 14:30:55 -0400

    feat - Creating a page to Update 'Ad' details with a reactive form and Image in Angular

--- service_booking_system_web/src/app/company/pages/update-ad/update-ad.component.ts
+++ service_booking_system_web/src/app/company/pages/update-ad/update-ad.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CompanyService } from '../../services/company.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-ad',
@@ -11,19 +13,71 @@ export class UpdateAdComponent {
 
   adId: any = this.activateRoute.snapshot.params['id'];
 
-  constructor(
-    private companyService: CompanyService,
-    private activateRoute: ActivatedRoute
-  ){}
+  selectedFile: File | null;
+  imagePreview: string | ArrayBuffer | null;
+  validateForm: FormGroup;
+  existingImage: string | null = null;
 
-  ngOnInit(){
+  imgChanged = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private notification: NzNotificationService,
+    private router: Router,
+    private companyService: CompanyService,
+    private activateRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      serviceName: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      price: [null, [Validators.required]],
+    })
     this.getAdById();
   }
 
-  getAdById(){
-    this.companyService.getAdById(this.adId).subscribe(res => {
-      console.log(res);
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    this.previewImage();
+    this.existingImage = null;
+    this.imgChanged = true;
+  }
+
+  previewImage() {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    }
+    reader.readAsDataURL(this.selectedFile);
+  }
+
+  updateAd() {
+    const formData: FormData = new FormData();
+    if (this.imgChanged && this.selectedFile) {
+      formData.append('img', this.selectedFile);
+    }
+    formData.append('serviceName', this.validateForm.get('serviceName').value);
+    formData.append('description', this.validateForm.get('description').value);
+    formData.append('price', this.validateForm.get('price').value);
+
+    this.companyService.updateAd(this.adId, formData).subscribe(res => {
+      this.notification.success('SUCCESS', `Ad Updated Successfully`, { nzDuration: 5000 });
+      this.router.navigateByUrl('/company/ads');
+    }, error => {
+      this.notification.success('ERROR', `${error.error}`, { nzDuration: 5000 });
+
     })
   }
 
+  getAdById() {
+    this.companyService.getAdById(this.adId).subscribe(res => {
+      console.log(res);
+      this.validateForm.patchValue(res);
+      this.existingImage = 'data:image/jpeg;base64,' + res.returnImg;
+    })
+  }
 }
+
+
+
